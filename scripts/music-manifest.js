e55a14a680c4c63a457ce7cd7118f6125fb0eae5
Lucifer <manhhohoang11@gmail.com>
2026-06-14 12:59:45 +0700

    ...

--- scripts/music-manifest.js
+++ scripts/music-manifest.js
@@ -1,1 +1,14 @@
-window.__PORTFOLIO_TRACKS__ = [];
+window.__PORTFOLIO_TRACKS__ = [
+  {
+    "src": "assets/music/Come%20my%20way.mp3",
+    "title": "SON TUNG M-TP x TYGA | COME MY WAY | OFFICIAL MUSIC VIDEO",
+    "artist": "Sơn Tùng M-TP Official",
+    "album": "SON TUNG M-TP x TYGA | COME MY WAY | OFFICIAL MUSIC VIDEO"
+  },
+  {
+    "src": "assets/music/TH%E1%BA%AENG%20-%20G%E1%BB%98I%20%C4%90%E1%BA%A6U%20ft.%20H%C3%A0%20L%C3%AA%20(Prod.%20RacoonOfficial).mp3",
+    "title": "THẮNG - GỘI ĐẦU ft. Hà Lê (Prod. RacoonOfficial) | Lyrics Video",
+    "artist": "Racoon Official",
+    "album": "THẮNG - GỘI ĐẦU ft. Hà Lê (Prod. RacoonOfficial) | Lyrics Video"
+  }
+];
